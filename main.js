29d1042c557bae3c76fe21eb2e21a151a0a7f92c
Ognjen Jevremovic <jevremovic.ognjen@gmail.com>
2022-03-23 13:54:46 +0100

    feat: 🎸 Nodes and paths extraction, from graph string
    Provide a functionality of extracting list of nodes on the graph, as
    well as the funcionality of constructing edges of the graph between
    nodes, from provided string input

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+/**
+ * These are the provided test cases, that mimmick
+ * the user input from the UI text element.
+ */
 //  Simple connected graph (tree), valid
 const testInput_one = 'a - b - c';
 //  Multiple unconnected graphs, invalid
@@ -15,14 +19,79 @@ const testInput_six = 'a - a';
 /**
  * @details
  *  Check if a graph is red-blue colorable.
+ * 
  * @public
- * @todo    Implement the algorithm for checking if a graph is two (red-blue) colorable.
- * @param   {*} input 
- * @returns {boolean}
+ * @param     {string} input - string representation of the graph(s)
+ * @returns   {boolean}
  */
 function checkGraph(input) {
+  // TODO: Do some initial validation of an input
+
+
+  //  Cover the case of both:
+  //    - Single graphs,
+  //    - Multiple graphs.
+  const paths = getGraphs(input).map(graph => getPathsFromInput(graph)).flat();
+  const nodes = getNodesFromInput(input);
+
+  console.log('paths', paths);
+  console.log('nodes', nodes);
+
   //  TODO: Implementation details
   return false;
+
+
+  /**
+   * @details
+   *  Provided a string representation of the graph(s)
+   *  return an Array of strings.
+   *  Each element of an array, represents a graph with nodes and edges.
+   * 
+   * @private
+   * @param     {string} graphString - string representation of the undirected graph, with nodes and edges. 
+   * @returns   {string[]}
+   */
+  function getGraphs(graphString) {
+    return graphString.replace(/\s/g, '').split(',');
+  }
+
+
+  /**
+   * @details
+   *  Find the graph's neighbouring nodes, through paths / edges.
+   * 
+   * @private
+   * @param     {string[]} graphs - string array representation of undirected graph(s)  
+   * @returns   {Array.<string[]>}
+   */
+  function getPathsFromInput(graphsArray) {
+    return graphsArray.replace(/\s/g, '').split('-')
+      .reduce((edges, node, idx, originalGraphArray) => {
+        const neighbourNode = originalGraphArray[idx + 1];
+
+        if (!!neighbourNode) {
+          edges = [...edges, [node, neighbourNode]];
+        }
+
+        return edges;
+      }, [])
+  }
+
+
+  /**
+   * @details
+   *  Extract the graph nodes, from the string representation of the graph(s).
+   * 
+   * @private
+   * @param     {string} graphString - string representation of the undirected graph, with nodes and edges. 
+   * @returns   {string[]}
+   */
+  function getNodesFromInput(graphString) {
+    return graphString.replace(/\s/g, '').split(',')
+      .map(val => val.split('-'))
+      .flat()
+      .filter((val, idx, arr) => arr.indexOf(val) === idx)
+  }
 }
 
 
